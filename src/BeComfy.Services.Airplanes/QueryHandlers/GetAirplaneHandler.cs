a3a113dbbf974a2004f268f12ade2a0973cfb600
serfin <gracjanbrylka@gmail.com>
2019-11-24 00:13:55 +0100

    Added GetAirplane query handler

---
+++ src/BeComfy.Services.Airplanes/QueryHandlers/GetAirplaneHandler.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using BeComfy.Common.CqrsFlow.Handlers;
+using BeComfy.Services.Airplanes.Dto;
+using BeComfy.Services.Airplanes.Queries;
+using BeComfy.Services.Airplanes.Repositories;
+
+namespace BeComfy.Services.Airplanes.QueryHandlers
+{
+    public class GetAirplaneHandler : IQueryHandler<GetAirplane, AirplaneDto>
+    {
+        private readonly IAirplanesRepository _repository;
+
+        public GetAirplaneHandler(IAirplanesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<AirplaneDto> HandleAsync(GetAirplane query)
+        {
+            var airplane = await _repository.GetAirplaneAsync(query.Id);
+
+            if (airplane != null)
+            {
+                return new AirplaneDto()
+                {
+                    Id = airplane.Id,
+                    Model = airplane.Model,
+                    AirplaneStatus = airplane.AirplaneStatus,
+                    NextFlight = airplane.NextFlight,
+                    FlightEnd = airplane.FlightEnd
+                };
+            }
+
+            return null;
+        }
+    }
+}
